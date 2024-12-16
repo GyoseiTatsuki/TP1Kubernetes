3bb7d338171c3a131104636b623cd54b53658f92
chrisn237 <chrisnassi237@gmail.com>
2024-12-16 17:45:37 +0100

    Initial commit

--- project/app.py
+++ project/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask # type: ignore
 
 from project.api import api
 
@@ -7,3 +7,5 @@ def create_app():
     app = Flask(__name__)
     api.init_app(app)
     return app
+
+POSTGRES_URL = "postgresql://appcrud:1234321@postgres-db.database.svc.cluster.local:5432/messages"
